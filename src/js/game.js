b6858994fc234ddeab673de133b54ea75d40c31a
Vladimir Zhukov <zhukovvlad@yandex.ru>
2019-12-01 23:07:59 +0300

    3rd task is completed

---
+++ src/js/game.js
@@ -0,0 +1,14 @@
+export default class Game {
+    start() {
+      console.log('game started');
+    }
+  }
+  
+  class GameSavingData {
+  }
+  
+  function readGameSaving() {
+  }
+  
+  function writeGameSaving() {
+  }
